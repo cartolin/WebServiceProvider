d937d88a59dac105563303e91ad3a010e1ff76c7
cartolin <cartolin.fidel@gmail.com>
2022-07-26 18:14:37 -0500

    DataContext Configuratio

---
+++ Src/Infrastructure/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+    }
+}
